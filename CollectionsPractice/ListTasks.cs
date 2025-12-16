c4cb95e69803985f3dd0395733f3394aa05444fe
Rizruf <sova_nikeo@mail.ru>
2025-12-16 16:47:28 +0600

    Выполнено слияние списков через коллекцию List<>

---
+++ CollectionsPractice/ListTasks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsPractice
+{
+    internal class ListTasks
+    {
+        public void ListMerge()
+        {
+            List<int> l1 = new List<int>() { 1, 2, 3 };
+            List<int> l2 = new List<int>() { 2, 3, 4, 5 };
+
+            List<int> merges = new List<int>();
+
+            merges.AddRange(l1);
+            merges.AddRange(l2);
+
+            foreach (int item in merges)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+
+            List <int> final = merges.Distinct().ToList(); ;
+
+            foreach (int item in final)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+
+        
+    }
+}
---
+++ CollectionsPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CollectionsPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListTasks myWorker = new ListTasks();
+
+            myWorker.ListMerge();
+
+            Console.WriteLine("\nНажми Enter, чтобы закрыть...");
+            Console.ReadLine();
+        }
+    }
+}
